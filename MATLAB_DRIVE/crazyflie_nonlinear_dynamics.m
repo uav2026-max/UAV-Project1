ada04551f7da197847bc513519c229ea34b9083b
uav2026-max <uav2026@gmail.com>
2026-03-27 14:30:29 +0000

    Add files via upload

---
+++ MATLAB_DRIVE/crazyflie_nonlinear_dynamics.m
@@ -0,0 +1,102 @@
+function dx = crazyflie_nonlinear_dynamics(~, x, u_rotors, params)
+% x = [px py pz u v w phi theta psi p q r]'
+% u_rotors = [T1 T2 T3 T4]'
+
+%% States
+px    = x(1);
+py    = x(2);
+pz    = x(3);
+ub    = x(4);
+vb    = x(5);
+wb    = x(6);
+phi   = x(7);
+theta = x(8);
+psi   = x(9);
+p     = x(10);
+q     = x(11);
+r     = x(12);
+
+v_b   = [ub; vb; wb];
+omega = [p; q; r];
+
+%% Parameters
+m      = params.m;
+g      = params.g;
+J      = params.J;
+l      = params.l;
+alpha  = params.alpha;
+kQ_kT  = params.kQ_kT;
+cDx    = params.cDx;
+cDy    = params.cDy;
+cDz    = params.cDz;
+
+%% Rotor thrusts
+T1 = u_rotors(1);
+T2 = u_rotors(2);
+T3 = u_rotors(3);
+T4 = u_rotors(4);
+
+%% Rotation matrix R_B^I = Rz(psi)*Ry(theta)*Rx(phi)
+cphi = cos(phi);   sphi = sin(phi);
+cth  = cos(theta); sth  = sin(theta);
+cpsi = cos(psi);   spsi = sin(psi);
+
+R = [cpsi*cth,  cpsi*sth*sphi - spsi*cphi,  cpsi*sth*cphi + spsi*sphi;
+     spsi*cth,  spsi*sth*sphi + cpsi*cphi,  spsi*sth*cphi - cpsi*sphi;
+     -sth,      cth*sphi,                   cth*cphi];
+
+%% Skew-symmetric matrix
+S_omega = [  0  -r   q;
+             r   0  -p;
+            -q   p   0];
+
+%% 1) Kinematics: position
+p_dot = R * v_b;
+
+%% 2) Forces in body frame
+
+% Gravity in body frame
+f_g = R' * [0; 0; -m*g];
+% Equivalent to:
+% f_g = [m*g*sin(theta);
+%       -m*g*cos(theta)*sin(phi);
+%       -m*g*cos(theta)*cos(phi)];
+
+% Quadratic drag in body frame
+f_a = [-cDx*ub*abs(ub);
+       -cDy*vb*abs(vb);
+       -cDz*wb*abs(wb)];
+
+% Propulsion force in body frame
+T_total = T1 + T2 + T3 + T4;
+f_p = [0; 0; T_total];
+
+%% 3) Moments from propulsion
+
+% Using your report expressions
+n_x = l*sin(alpha) * (-T1 - T2 + T3 + T4);
+n_y = l*cos(alpha) * (-T1 + T2 + T3 - T4);
+n_z = kQ_kT * (-T1 + T2 - T3 + T4);
+
+n_p = [n_x; n_y; n_z];
+
+%% 4) Translational dynamics
+v_dot = -S_omega * v_b + (1/m) * (f_g + f_a + f_p);
+
+%% 5) Euler angle kinematics
+% lambda_dot = Q(lambda)*omega
+Q = [1,  sphi*tan(theta),  cphi*tan(theta);
+     0,  cphi,            -sphi;
+     0,  sphi/cth,         cphi/cth];
+
+euler_dot = Q * omega;
+
+%% 6) Rotational dynamics
+omega_dot = -J \ (S_omega * J * omega) + J \ n_p;
+
+%% Final state derivative
+dx = [p_dot;
+      v_dot;
+      euler_dot;
+      omega_dot];
+end
---
+++ MATLAB_DRIVE/main_P15.m
@@ -0,0 +1,98 @@
+clear; clc; close all;
+
+%% Parameters
+params.m  = 0.027;                 % mass [kg] (Crazyflie 2.1 approx.)
+params.g  = 9.81;                  % gravity [m/s^2]
+
+% Inertia matrix [kg.m^2] (approximate values, adjust if you found better ones)
+params.J  = diag([1.4e-5 1.4e-5 2.17e-5]);
+
+% Geometry
+params.l     = 0.0397;             % distance from CoM to rotor [m]
+params.alpha = pi/4;               % 45 deg arm geometry
+
+% Rotor drag/thrust ratio for yaw moment
+params.kQ_kT = 0.006;              % adjust if you found a better value
+
+% Aerodynamic drag coefficients in body axes
+params.cDx = 0.0;
+params.cDy = 0.0;
+params.cDz = 0.0;
+% If you want drag effects, try e.g.:
+% params.cDx = 2e-4; params.cDy = 2e-4; params.cDz = 3e-4;
+
+%% Input: equal constant thrust on all rotors
+T_hover_total = params.m * params.g;
+T_hover_each  = T_hover_total / 4;
+
+% "small constant thrust, equal to all rotors"
+% Try one of these:
+% T_each = 0.95*T_hover_each;   % slightly below hover -> drone descends
+% T_each = 1.00*T_hover_each;   % ideal hover
+T_each = 1.05*T_hover_each;     % slightly above hover -> drone climbs
+
+u = [T_each; T_each; T_each; T_each];
+
+%% Initial condition: resting state
+% x = [px py pz u v w phi theta psi p q r]'
+x0 = zeros(12,1);
+
+%% Simulation time
+tspan = [0 5];
+
+%% Integrate nonlinear model
+[t, x] = ode45(@(t,x) crazyflie_nonlinear_dynamics(t, x, u, params), tspan, x0);
+
+%% Extract states
+px    = x(:,1);  py    = x(:,2);  pz    = x(:,3);
+ub    = x(:,4);  vb    = x(:,5);  wb    = x(:,6);
+phi   = x(:,7);  theta = x(:,8);  psi   = x(:,9);
+p     = x(:,10); q     = x(:,11); r     = x(:,12);
+
+%% Plots
+figure;
+plot(t, px, 'LineWidth', 1.5); hold on;
+plot(t, py, 'LineWidth', 1.5);
+plot(t, pz, 'LineWidth', 1.5);
+grid on;
+xlabel('Time [s]');
+ylabel('Position [m]');
+legend('p_x','p_y','p_z');
+title('Position in inertial frame');
+
+figure;
+plot(t, ub, 'LineWidth', 1.5); hold on;
+plot(t, vb, 'LineWidth', 1.5);
+plot(t, wb, 'LineWidth', 1.5);
+grid on;
+xlabel('Time [s]');
+ylabel('Body linear velocity [m/s]');
+legend('u','v','w');
+title('Body-frame linear velocities');
+
+figure;
+plot(t, phi*180/pi, 'LineWidth', 1.5); hold on;
+plot(t, theta*180/pi, 'LineWidth', 1.5);
+plot(t, psi*180/pi, 'LineWidth', 1.5);
+grid on;
+xlabel('Time [s]');
+ylabel('Euler angles [deg]');
+legend('\phi','\theta','\psi');
+title('Euler angles');
+
+figure;
+plot(t, p, 'LineWidth', 1.5); hold on;
+plot(t, q, 'LineWidth', 1.5);
+plot(t, r, 'LineWidth', 1.5);
+grid on;
+xlabel('Time [s]');
+ylabel('Angular velocity [rad/s]');
+legend('p','q','r');
+title('Body angular velocities');
+
+%% Simple 3D trajectory
+figure;
+plot3(px, py, pz, 'LineWidth', 2);
+grid on; axis equal;
+xlabel('x [m]'); ylabel('y [m]'); zlabel('z [m]');
+title('3D trajectory');
